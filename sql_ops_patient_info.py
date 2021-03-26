c068938125a9336a02d201c984c120b1d2b42a56
shweta_pccm <shwetakadu.prashanticancercare@gmail.com>
2021-03-26 17:35:13 +0530

    sqlalchemy

--- sql_ops_patient_info.py
+++ sql_ops_patient_info.py
@@ -55,7 +55,7 @@ patient_info_phy_act_stat = "UPDATE patient_information_history SET type_physica
 # phy_act = pd.concat([old_phy_act, updated_phy_act], axis=1)
 ##
 
-physical_activity_dict = {'walking': ['walk', 'walking', 'walking_for_exercise', 'lawn_walking', 'lawn moving', 'lawn mowing',
+physical_activity_dict = {'walking': ['walk', 'walking', 'walking_for_exercise', 'lawn walking', 'lawn moving', 'lawn mowing', 'lawn',
                                       'walking for exercise'],
                              'cycling': ['cycle', 'cycling', 'bicycling'],
                              'running': ['run', 'running'],
@@ -64,10 +64,10 @@ physical_activity_dict = {'walking': ['walk', 'walking', 'walking_for_exercise',
                              'gym': ['gym', 'gymming'],
                              'dancing': ['dance', 'dancing', 'kathak', 'zumba'],
                              'exercise': ['exercise'],
-                             'lower_intensity_exercise': ['lower intensity exercise', 'intensity'],
+                             'lower_intensity_exercise': ['lower intensity exercise'],
                              'aerobic_exercise': ['aerobic exercise', 'other aerobic exercise'],
                              'yoga': 'yoga',
-                             'player': ['badminton', 'tennis', 'throw ball'],
+                             'playing': ['badminton', 'tennis', 'throw ball', 'ball'],
                              'requires_follow_up': ['requires_follow_up', 'requires follow-up', 'requires follow up'],
                              'no_physical_activities': ['no_physical_activities', 'no physical activities'],
                              'data_not_available': ['data not available', 'data_not_available']
@@ -88,8 +88,8 @@ def replace_and_split_string(column):
 
 phy_act = patient_info['type_physical_activity'].str.lower()
 vals_dict = phy_act.to_dict()
-
 # phy_act_splitted = replace_and_split_string(phy_act)
+
 # phy_act_splitted_sr = pd.Series(phy_act_splitted)
 # vals_dict = phy_act_splitted_sr.to_dict()
 
@@ -120,69 +120,88 @@ def repalce_values_by_dict_keys(physical_activity_dict, dict_values):
                     cleaned_value = re.sub('[^a-zA-Z]', '', value)
                     cleaned_value = cleaned_value.lower()
                     key_reqd = get_value_from_key(physical_activity_dict, cleaned_value)
-                    #print(key_reqd)
+                    # print(key_reqd)
                     if key_reqd is not None:
                         key_reqd_str = ';'.join(key_reqd)
                         lst.append(key_reqd_str)
                         # print(lst)
                         while ('' in lst):
                             lst.remove('')
-                            changed_phy_act.append(lst)
+                            #changed_phy_act.append(lst)
                     else:
                         lst.append(key_reqd)
                        #print(key_reqd_str)
+                print(lst)
+                changed_phy_act.append(lst)
         else:
             changed_phy_act.append('data_not_available')
     return changed_phy_act, lst
 
 changed_phy_acts_lst, lst = repalce_values_by_dict_keys(physical_activity_dict, vals_dict)
 
-cleaned_lst = []
-for lst in changed_phy_acts_lst:
-    print(len(lst))
-    print(lst)
-    while ('' in lst):
-        lst_clean = lst.remove('')
-        print(lst_clean)
-        cleaned_lst.append(lst_clean)
+changed_phy_acts_sr = pd.Series(changed_phy_acts_lst)
+phy_act_df = pd.concat([patient_info['type_physical_activity'], changed_phy_acts_sr], axis=1)
+
+# phy_act_df.to_excel('D:\Shweta\pccm_db\output_df\\2021_03_26_physical_activities_sk.xlsx', index=False)
+
+# cleaned_lst = []
+# for lst in changed_phy_acts_lst:
+#     print(len(lst))
+#     print(lst)
+#     while ('' in lst):
+#         lst_clean = lst.remove('')
+#         print(lst_clean)
+#         cleaned_lst.append(lst_clean)
 
 
-
-phy_act_lst = list(patient_info['type_physical_activity'])
-patient_info['type_physical_activity'] = patient_info['type_physical_activity'].replace(phy_act_lst, changed_phy_acts_lst)
+# phy_act_lst = list(patient_info['type_physical_activity'])
+# patient_info['type_physical_activity'] = patient_info['type_physical_activity'].replace(phy_act_lst, changed_phy_acts_lst)
 dat = ['Walking; Yoga', 'Yoga; Kathak Dance', 'Walking; Jogging; Bicycling; Lap. Swimming; Aerobic Exercise; Yoga; Lawn Mowing']
-strings = 'daily exercise and yoga'
-splitted_strings = []
+strings = 'Walking; Jogging; Bicycling; Lap. Swimming; Aerobic Exercise; Yoga; Lawn Mowing'
+
+from nltk import everygrams
+
+msg_split = strings.split()
+
+split_cleaned_lst = []
+for value in msg_split:
+    split = re.sub('[^a-zA-Z]', '', value)
+    split_cleaned = split.lower()
+    split_cleaned_lst.append(split_cleaned)
+    print(split_cleaned)
+
+list(everygrams(split_cleaned_lst))
+
 # for string in strings:
 #     splitted_str = string.split()
 #     splitted_strings.append(splitted_str)
 #     print(splitted_str)
 
-dat = 'Walking; Jogging; Bicycling; Lap. Swimming; Aerobic Exercise; Yoga; Lawn Mowing'
-splitted_strings  = dat.split()
+# dat = 'Walking; Jogging; Bicycling; Lap. Swimming; Aerobic Exercise; Yoga; Lawn Mowing'
+# splitted_strings  = dat.split()
 
-lst = []
-for value in splitted_strings:
-    #print(value)
-    cleaned_value = re.sub('[^a-zA-Z]', '', value)
-    cleaned_value = cleaned_value.lower()
-    #print(cleaned_value)
-    key_reqd = get_value_from_key(physical_activity_dict, cleaned_value)
-    print(key_reqd)
-    if key_reqd is not None:
-        key_reqd_str = ''.join(key_reqd)
-        #print(key_reqd_str)
-        lst.append(key_reqd_str)
-        while ('' in lst):
-            lst.remove('')
-            print(lst)
-    else:
-        lst.append(key_reqd)
-
-while ('' in lst):
-    lst.remove('')
-    print(cleaned_lst)
-        #print(key_reqd_str)
+# lst = []
+# for value in splitted_strings:
+#     #print(value)
+#     cleaned_value = re.sub('[^a-zA-Z]', '', value)
+#     cleaned_value = cleaned_value.lower()
+#     #print(cleaned_value)
+#     key_reqd = get_value_from_key(physical_activity_dict, cleaned_value)
+#     print(key_reqd)
+#     if key_reqd is not None:
+#         key_reqd_str = ''.join(key_reqd)
+#         #print(key_reqd_str)
+#         lst.append(key_reqd_str)
+#         while ('' in lst):
+#             lst.remove('')
+#             print(lst)
+#     else:
+#         lst.append(key_reqd)
+#
+# while ('' in lst):
+#     lst.remove('')
+#     print(cleaned_lst)
+#         print(key_reqd_str)
 
 # word = 'walk; jogging; running;bicycling; tennis'
 # patterns = ['walk', 'walking', 'jogging', 'running', 'bicycling', 'tennis']
--- sqlalchemy.py
+++ sqlalchemy.py
@@ -2,27 +2,79 @@ import sqlalchemy
 sqlalchemy.__version__
 from sqlalchemy import create_engine
 from sqlalchemy import MetaData, Table, Column, Integer, String, Boolean, Float
+from sqlalchemy import inspect
+import pandas as pd
+from sqlalchemy.sql import text
+import uuid
+
 
 # folder = 'D:/Shweta/pccm_db'
 # file = 'PCCM_BreastCancerDB_2021_02_22.db'
 # path_db = os.path.join(folder, file)
 
+
 engine = create_engine('sqlite:///D://Shweta//pccm_db//PCCM_BreastCancerDB_2021_02_22.db')
-meta = MetaData()
+metadata = MetaData()
+inspector = inspect(engine)
+
+conn = engine.connect()
 
 phy_act = Table(
-    'physical_activities', meta,
-    Column('pk' , String, primary_key = True),
+    'physical_activities', metadata,
+    Column('pk', String),
     Column('file_number', String),
     Column('phy_acy_y_n', Boolean),
     Column('phy_act_type', String),
     Column('phy_act_freq_in_hrs', Float)
 )
 
-# meta.create_all(engine)
+metadata.create_all(engine)
+metadata.tables['physical_activities']
+tabs = inspector.get_table_names()
+
+print(inspector.get_table_names())   # it gives tables names of db
+print(inspector.get_columns('physical_activities'))  # it gives col names of table
+print(metadata.tables)
+
+# phy_act_tab = conn.execute("SELECT * FROM physical_activities")
+phy_act_tab = pd.read_sql("SELECT * FROM 'physical_activities'", conn)
+patient_info = pd.read_sql("SELECT * FROM patient_information_history", conn)
+
+# patient_info.columns
+# len(patient_info)
+
+def generate_pk(df):
+    pks = []
+    for i in range(0, len(df)):
+        pk = uuid.uuid4().hex
+        pks.append(pk)
+    return pks
+
+
+def add_fk(df, key_col_name):
+    df[key_col_name] = generate_pk(df)
+    return df
+
+
+patient_info = add_fk(patient_info, 'pk')
+
+
+insert_stat = text("INSERT INTO physical_activities ('pk', 'file_number', 'phy_acy_y_n', 'phy_act_type', 'phy_act_freq_in_hrs') SELECT " \
+              "'pk', 'file_number', 'physical_activity_y_n', 'type_physical_activity', 'frequency_physical_activity' " \
+              "FROM patient_information_history")
+
+
+conn.execute(insert_stat)
+
+
+phy_act_tab = conn.execute("SELECT * FROM physical_activities")
+
+cursor = conn.cursor()
+
 # conn = engine.connect()
 #
 # sql_stat = text("SELECT * FROM sqlite_master WHERE TYPE = 'table'")
 # tabs = conn.execute(sql_stat)
 #
 # tables = pd.read_sql(sql_stat, conn)
+
