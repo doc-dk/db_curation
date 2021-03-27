f8c95e034a467c91c6198727610fad79807dc5e2
shweta_pccm <shwetakadu.prashanticancercare@gmail.com>
2021-03-27 16:20:51 +0530

    sqlalchemy

--- sqlalchemy.py
+++ sqlalchemy.py
@@ -6,6 +6,8 @@ from sqlalchemy import inspect
 import pandas as pd
 from sqlalchemy.sql import text
 import uuid
+import itertools
+import re
 
 
 # folder = 'D:/Shweta/pccm_db'
@@ -40,8 +42,10 @@ print(metadata.tables)
 phy_act_tab = pd.read_sql("SELECT * FROM 'physical_activities'", conn)
 patient_info = pd.read_sql("SELECT * FROM patient_information_history", conn)
 
-# patient_info.columns
-# len(patient_info)
+# phy_act_stat = "SELECT 'file_number', 'physical_activity_y_n', 'type_physical_activity', " \
+#                "'frequency_physical_activity' FROM patient_information_history"
+
+phy_act_data = patient_info[['file_number', 'physical_activity_y_n', 'type_physical_activity', 'frequency_physical_activity']]
 
 def generate_pk(df):
     pks = []
@@ -56,20 +60,99 @@ def add_fk(df, key_col_name):
     return df
 
 
-patient_info = add_fk(patient_info, 'pk')
+pks = generate_pk(phy_act_data)
+phy_act_df = add_fk(phy_act_data, 'pk')
+
+physical_activity_dict = {'walking': ['walk', 'walking', 'walking_for_exercise', 'lawn walking', 'lawn moving', 'lawn mowing', 'lawn',
+                                      'walking for exercise'],
+                             'cycling': ['cycle', 'cycling', 'bicycling'],
+                             'running': ['run', 'running'],
+                             'swimming': ['swim', 'swimming', 'lap swimming', 'seasonal swimming'],
+                             'jogging': ['jogging', 'jogg'],
+                             'gym': ['gym', 'gymming'],
+                             'dancing': ['dance', 'dancing', 'kathak', 'zumba'],
+                             'exercise': ['exercise'],
+                             'lower_intensity_exercise': ['lower intensity exercise'],
+                             'aerobic_exercise': ['aerobic exercise', 'other aerobic exercise'],
+                             'yoga': 'yoga',
+                             'playing': ['badminton', 'tennis', 'throw ball', 'ball'],
+                             'requires_follow_up': ['requires_follow_up', 'requires follow-up', 'requires follow up'],
+                             'no_physical_activities': ['no_physical_activities', 'no physical activities'],
+                             'data_not_available': ['data not available', 'data_not_available']
+                             }
+
+phy_act = phy_act_df['type_physical_activity'].str.lower()
+vals_dict = phy_act.to_dict()
+
+def get_value_from_key(vocab_dict, value):
+    id_pos = [value in value_list for value_list in (vocab_dict.values())]
+    key_reqd = list(itertools.compress(vocab_dict.keys(), id_pos))
+    return key_reqd
 
 
-insert_stat = text("INSERT INTO physical_activities ('pk', 'file_number', 'phy_acy_y_n', 'phy_act_type', 'phy_act_freq_in_hrs') SELECT " \
-              "'pk', 'file_number', 'physical_activity_y_n', 'type_physical_activity', 'frequency_physical_activity' " \
-              "FROM patient_information_history")
+def repalce_values_by_dict_keys(physical_activity_dict, dict_values):
+    changed_phy_act = []
+    for val in dict_values.values():
+        if val is not None:
+            vocab_type = get_value_from_key(physical_activity_dict, val)
+            if len(vocab_type) != 0:
+                changed_phy_act.append(vocab_type)
+            else:
+                split_val = val.split()
+                lst = []
+                for value in split_val:
+                    cleaned_value = re.sub('[^a-zA-Z]', '', value)
+                    cleaned_value = cleaned_value.lower()
+                    key_reqd = get_value_from_key(physical_activity_dict, cleaned_value)
+                    if key_reqd is not None:
+                        key_reqd_str = ';'.join(key_reqd)
+                        lst.append(key_reqd_str)
+                        while ('' in lst):
+                            lst.remove('')
+                    else:
+                        lst.append(key_reqd)
+                changed_phy_act.append(lst)
+        else:
+            changed_phy_act.append('data_not_available')
+    return changed_phy_act, lst
 
 
-conn.execute(insert_stat)
+changed_phy_act, lst = repalce_values_by_dict_keys(physical_activity_dict, vals_dict)
+
+# phy_act_lst = list(phy_act_df['type_physical_activity'])
+# phy_act_df['type_physical_activity'] = phy_act_df['type_physical_activity'].replace(phy_act_lst, changed_phy_act)
+
+phy_act_df['cleaned_physical_activity'] = pd.Series(changed_phy_act)
+
+# print(len(phy_act_lst), len(phy_act_df), len(changed_phy_act))
+
+# for phy_act_dat in phy_act_df['cleaned_physical_activity']:
+#     print(phy_act_dat)
+#     if len(phy_act_dat) == 1:
+#         cleaned_phy_act_dat = re.sub('[^a-zA-Z_0-9]', '', str(phy_act_dat))
+#         cleaned_phy_act_dat = cleaned_phy_act_dat.lower()
+#         print(cleaned_phy_act_dat)
+#     else:
+#         cleaned_phy_act_dat = re.sub('[^a-zA-Z_0-9]', '_', str(phy_act_dat))
+#         cleaned_phy_act_dat = cleaned_phy_act_dat.lower()
+#         cleaned_phy_act_dat = cleaned_phy_act_dat.split()
+#         print(cleaned_phy_act_dat)
+#         cleaned_phy_act_dat = cleaned_phy_act_dat.remove("__")
+#         cleaned_phy_act_dat = cleaned_phy_act_dat.replace("____", ";")
+#         print(cleaned_phy_act_dat)
 
 
-phy_act_tab = conn.execute("SELECT * FROM physical_activities")
+# insert_stat = text("INSERT INTO physical_activities ('pk', 'file_number', 'phy_acy_y_n', 'phy_act_type', 'phy_act_freq_in_hrs') SELECT " \
+#               "'pk', 'file_number', 'physical_activity_y_n', 'type_physical_activity', 'frequency_physical_activity' " \
+#               "FROM patient_information_history")
 
-cursor = conn.cursor()
+
+# conn.execute(insert_stat)
+
+
+# phy_act_tab = conn.execute("SELECT * FROM physical_activities")
+
+# cursor = conn.cursor()
 
 # conn = engine.connect()
 #
